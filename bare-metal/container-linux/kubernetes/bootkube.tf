46e965aca96567eedd0e1c4bd057796a30e85a21
Lars Fenneberg <lf@elemental.net>
2017-11-29 14:28:28 +0100

    New bootkube version.

--- bare-metal/container-linux/kubernetes/bootkube.tf
+++ bare-metal/container-linux/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/elemental-lf/terraform-render-bootkube.git?ref=336bbd515cf82a1cc2ae47b0515cbb4d5021c7b8"
+  source = "git::https://github.com/elemental-lf/terraform-render-bootkube.git?ref=acde10b26d95ad8d45618ca206374fb9a4eeeb57"
 
   cluster_name = "${var.cluster_name}"
   api_servers  = ["${var.k8s_domain_name}"]
