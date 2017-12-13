e1b039f8a459672cb8ff4c014f636e52eecd37ad
Lars Fenneberg <lf@elemental.net>
2017-12-13 21:18:35 +0100

    canal/flannel: make flannel setup more customisable
    * Make flannel interface settable. The default still is the pod IP.
    * Add variable flannel_backend to set the flannel backend. The default still
    is vxlan.

--- bare-metal/container-linux/kubernetes/bootkube.tf
+++ bare-metal/container-linux/kubernetes/bootkube.tf
@@ -1,13 +1,15 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/elemental-lf/terraform-render-bootkube.git?ref=eb60d2248f7fc1320113064be96bccd2f30506b1"
+  source = "git::https://github.com/elemental-lf/terraform-render-bootkube.git?ref=5fd3f0f34c543d9ed7e04916ac3bb45173026090"
 
-  cluster_name = "${var.cluster_name}"
-  api_servers  = ["${var.k8s_domain_name}"]
-  etcd_servers = ["${var.controller_domains}"]
-  asset_dir    = "${var.asset_dir}"
-  networking   = "${var.networking}"
-  network_mtu  = "${var.network_mtu}"
-  pod_cidr     = "${var.pod_cidr}"
-  service_cidr = "${var.service_cidr}"
+  cluster_name    = "${var.cluster_name}"
+  api_servers     = ["${var.k8s_domain_name}"]
+  etcd_servers    = ["${var.controller_domains}"]
+  asset_dir       = "${var.asset_dir}"
+  networking      = "${var.networking}"
+  network_mtu     = "${var.network_mtu}"
+  pod_cidr        = "${var.pod_cidr}"
+  service_cidr    = "${var.service_cidr}"
+  flannel_backend = "${var.flannel_backend}"
+  flannel_iface   = "${var.flannel_iface}"
 }
