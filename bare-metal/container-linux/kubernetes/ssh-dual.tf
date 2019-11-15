68fe12c4e45903a5ce4736184550b2686bdc15e9
Lars Fenneberg <lf@elemental.net>
2019-11-15 17:35:25 +0100

    Prevent copy-extra-assets from running on non-reinstalled master nodes

--- bare-metal/container-linux/kubernetes/ssh-dual.tf
+++ bare-metal/container-linux/kubernetes/ssh-dual.tf
@@ -14,6 +14,10 @@ resource "null_resource" "copy-extra-assets" {
 
   provisioner "remote-exec" {
     inline = [
+      # This is for the migration scenario from bootkube to bootstrap.
+      # Terraform will create this resource because it is missing but due to the fact that the nodes are not
+      # reinstalled yet this would fail.
+      "[[ -d /opt/bootstrap/assets/extra-assets ]] || exit 0",
       "sudo mkdir -p /etc/kubernetes/extra-assets",
       "sudo cp -r /opt/bootstrap/assets/extra-assets/* /etc/kubernetes/extra-assets/",
       "sudo chmod a+x /etc/kubernetes/extra-assets/apiserver-vip/*.sh",
