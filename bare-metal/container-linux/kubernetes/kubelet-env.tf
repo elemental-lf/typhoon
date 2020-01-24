424b2b0fcc56b0c241f2cee815a41c996fa95f4e
Lars Fenneberg <lf@elemental.net>
2020-01-24 17:26:38 +0100

    Add missing file kubelet-env.tf
    This file is missing from commit e3d0af17.

---
+++ bare-metal/container-linux/kubernetes/kubelet-env.tf
@@ -0,0 +1,6 @@
+locals {
+  kubelet_env = <<-EOT
+  KUBELET_IMAGE_URL=docker://${split(":", var.container_images["kubelet"])[0]}
+  KUBELET_IMAGE_TAG=${split(":", var.container_images["kubelet"])[1]}
+  EOT
+}
